dbde4abffb3ab2d6624e0c386e95f983df0d50eb
johnatm <john.atm@gmail.com>
2016-05-14 12:08:08 +0530

    Final commit

---
+++ SensorTagLibraryRealDLL/BleIRTemperatureService.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using Buffer = Windows.Storage.Streams.Buffer;
+
+
+namespace UnityPluginForWindowsPhone
+{
+
+
+
+    public class BleIRTemperatureService : BleGenericGattService
+    {
+
+        public BleIRTemperatureService()
+        {
+        }
+
+        /// <summary>
+        /// The version of the SensorTag device.  1=CC2541, 2=CC2650.
+        /// </summary>
+        public int Version { get; set; }
+
+        public static Guid IRTemperatureServiceUuid = Guid.Parse("f000aa00-0451-4000-b000-000000000000");
+        static Guid IRTemperatureCharacteristicUuid = Guid.Parse("f000aa01-0451-4000-b000-000000000000");
+        static Guid IRTemperatureCharacteristicConfigUuid = Guid.Parse("f000aa02-0451-4000-b000-000000000000");
+        static Guid IRTemperatureCharacteristicPeriodUuid = Guid.Parse("f000aa03-0451-4000-b000-000000000000");
+
+        Delegate _irTemperatureValueChanged;
+
+        public event EventHandler<IRTemperatureMeasurementEventArgs> IRTemperatureMeasurementValueChanged
+        {
+            add
+            {
+                if (_irTemperatureValueChanged != null)
+                {
+                    _irTemperatureValueChanged = Delegate.Combine(_irTemperatureValueChanged, value);
+                }
+                else
+                {
+                    _irTemperatureValueChanged = value;
+                    RegisterForValueChangeEvents(IRTemperatureCharacteristicUuid);
+                }
+            }
+            remove
+            {
+                if (_irTemperatureValueChanged != null)
+                {
+                    _irTemperatureValueChanged = Delegate.Remove(_irTemperatureValueChanged, value);
+                    if (_irTemperatureValueChanged == null)
+                    {
+                        UnregisterForValueChangeEvents(IRTemperatureCharacteristicUuid);
+                    }
+                }
+            }
+        }
+
+#if FALSE
+
+        // the documentation lies, the IR temp has no period characteristic
+
+        /// <summary>
+        /// Get the rate at which accelerometer is being polled, in milliseconds.  
+        /// </summary>
+        /// <returns>Returns the value read from the sensor or -1 if something goes wrong.</returns>
+        public async Task<int> GetPeriod()
+        {
+            byte v = await ReadCharacteristicByte(IRTemperatureCharacteristicPeriodUuid, Windows.Devices.Bluetooth.BluetoothCacheMode.Uncached);
+            return (int)(v * 10);
+        }
+
+        /// <summary>
+        /// Set the rate at which accelerometer is being polled, in milliseconds.  
+        /// </summary>
+        /// <param name="milliseconds">The delay between updates, accurate only to 10ms intervals. </param>
+        public async void SetPeriod(int milliseconds)
+        {
+            int delay = milliseconds / 10;
+            if (delay < 0)
+            {
+                delay = 1;
+            }
+
+            await WriteCharacteristicByte(IRTemperatureCharacteristicPeriodUuid, (byte)delay);
+        }
+#endif
+
+        bool isReading;
+
+        public async Task StartReading()
+        {
+            if (!isReading)
+            {
+                await WriteCharacteristicByte(IRTemperatureCharacteristicConfigUuid, 1);
+                isReading = true;
+            }
+        }
+
+        public async Task StopReading()
+        {
+            if (isReading)
+            {
+                isReading = false;
+                await WriteCharacteristicByte(IRTemperatureCharacteristicConfigUuid, 0);
+            }
+        }
+
+        private void OnIRTemperatureMeasurementValueChanged(IRTemperatureMeasurementEventArgs args)
+        {
+            if (_irTemperatureValueChanged != null)
+            {
+                ((EventHandler<IRTemperatureMeasurementEventArgs>)_irTemperatureValueChanged)(this, args);
+            }
+        }
+
+
+        public async Task<bool> ConnectAsync(string deviceContainerId)
+        {
+            return await this.ConnectAsync(IRTemperatureServiceUuid, deviceContainerId);
+        }
+
+        protected override void OnCharacteristicValueChanged(GattCharacteristic sender, GattValueChangedEventArgs eventArgs)
+        {
+            if (sender.Uuid == IRTemperatureCharacteristicUuid)
+            {
+                if (_irTemperatureValueChanged != null)
+                {
+                    uint dataLength = eventArgs.CharacteristicValue.Length;
+                    using (DataReader reader = DataReader.FromBuffer(eventArgs.CharacteristicValue))
+                    {
+                        if (dataLength == 4)
+                        {
+                            var data = new byte[dataLength];
+                            reader.ReadBytes(data);
+
+                            IRTemperatureMeasurement measurement = new IRTemperatureMeasurement();
+                            ushort objTemp = (ushort)(data[0] + (data[1] << 8));
+                            ushort dieTemp = (ushort)(data[2] + (data[3] << 8));
+
+                            if (Version == 1)
+                            {
+                                measurement.DieTemperature = dieTemp / 128.0;
+
+                                double Vobj2 = (double)objTemp;
+
+                                Vobj2 *= 0.00000015625;
+
+                                double Tdie = measurement.DieTemperature + 273.15;
+
+                                double S0 = 7E-14;	// Calibration factor: todo: this factor needs to be computed via calibration step.
+                                double a1 = 1.75E-3;
+                                double a2 = -1.678E-5;
+                                double b0 = -2.94E-5;
+                                double b1 = -5.7E-7;
+                                double b2 = 4.63E-9;
+                                double c2 = 13.4;
+                                double Tref = 298.15;
+                                double S = S0 * (1 + a1 * (Tdie - Tref) + a2 * Math.Pow((Tdie - Tref), 2));
+                                double Vos = b0 + b1 * (Tdie - Tref) + b2 * Math.Pow((Tdie - Tref), 2);
+                                double fObj = (Vobj2 - Vos) + c2 * Math.Pow((Vobj2 - Vos), 2);
+                                double tObj = Math.Pow(Math.Pow(Tdie, 4) + (fObj / S), .25);
+
+                                measurement.ObjectTemperature = (tObj - 273.15);
+                            }
+                            else
+                            {
+                                const double SCALE_LSB = 0.03125;
+                                int it;
+
+                                it = (int)((objTemp) >> 2);
+                                measurement.ObjectTemperature = (double)it * SCALE_LSB;
+
+                                it = (int)((dieTemp) >> 2);
+                                measurement.DieTemperature = (double)it * SCALE_LSB;
+
+                            }
+                            OnIRTemperatureMeasurementValueChanged(new IRTemperatureMeasurementEventArgs(measurement, eventArgs.Timestamp));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public class IRTemperatureMeasurement
+    {
+        public double ObjectTemperature
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Temperature measured on the die where sensor is mounted.
+        /// </summary>
+        public double DieTemperature
+        {
+            get;
+            set;
+        }
+
+        public IRTemperatureMeasurement()
+        {
+        }
+
+
+    }
+
+    public class IRTemperatureMeasurementEventArgs : EventArgs
+    {
+        public IRTemperatureMeasurementEventArgs(IRTemperatureMeasurement measurement, DateTimeOffset timestamp)
+        {
+            Measurement = measurement;
+            Timestamp = timestamp;
+        }
+
+        public IRTemperatureMeasurement Measurement
+        {
+            get;
+            private set;
+        }
+
+        public DateTimeOffset Timestamp
+        {
+            get;
+            private set;
+        }
+    }
+
+}
---
+++ SensorTagLibraryRealDLL/SensorValuesHelp.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityPluginForWindowsPhone
+{
+   public class SensorValuesHelp
+    {
+
+        public static string status = "none";
+        static SensorTag sensor;
+        static int? period;
+
+        public static void Start()
+        {
+            sensor = SensorTag.SelectedSensor;
+            StartSenser();
+        }
+
+        private static async void StartSenser()
+        {          
+            ShowMessage("Connecting...");
+            try
+            {
+                if (sensor.Version == 1)
+                {
+                    await sensor.Accelerometer.StartReading();
+                    sensor.Accelerometer.AccelerometerMeasurementValueChanged += OnAccelerometerMeasurementValueChanged;
+                    period = await sensor.Accelerometer.GetPeriod();
+                }
+                else if (sensor.Version == 2)
+                {
+                    await sensor.Movement.StartReading(MovementFlags.Accel2G | MovementFlags.AccelX | MovementFlags.AccelY | MovementFlags.AccelZ);
+                    sensor.Movement.MovementMeasurementValueChanged += OnMovementMeasurementValueChanged;
+                    period = await sensor.Movement.GetPeriod();
+                }
+                //SetSensitivity(period.Value);
+                ShowMessage("");
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private static void ShowMessage(string v)
+        {
+            status = v;
+        }
+
+        double GetSensitivity()
+        {
+            // Slider goes from 100 to 2550 for the "period" where 2550 is the slowest and therefore the least sensitive.
+            // Therefore we need to reverse the slider values in order for the slider to control sensitivity.
+            return 2250;
+        }
+
+        void SetSensitivity(double value)
+        {
+            // Slider is reversed.  See GetSensitivity.
+            
+        }
+
+ 
+
+        public static void StopSensor()
+        {
+            
+            if (sensor.Accelerometer != null)
+            {
+                sensor.Accelerometer.AccelerometerMeasurementValueChanged -= OnAccelerometerMeasurementValueChanged;
+            }
+            else if (sensor.Movement != null)
+            {
+                sensor.Movement.MovementMeasurementValueChanged -= OnMovementMeasurementValueChanged;
+            }
+            //await sensor.Accelerometer.StopReading();
+        }
+
+        static AccelerometerMeasurement measurement;
+        static MovementMeasurement movement;
+
+        private static void OnAccelerometerMeasurementValueChanged(object sender, AccelerometerMeasurementEventArgs e)
+        {
+            measurement = e.Measurement;            
+        }
+
+        static void OnMovementMeasurementValueChanged(object sender, MovementEventArgs e)
+        {
+            movement = e.Measurement;            
+        }
+
+        public static double GetX()
+        {
+            if (measurement != null)
+            {
+                return measurement.X;
+            }
+            return 0;
+        }
+
+        public static double GetY()
+        {
+            if (measurement != null)
+            {
+                return measurement.Y;
+            }
+            return 0;
+        }
+
+        public static double GetZ()
+        {
+            if (measurement != null)
+            {
+                return measurement.Y;
+            }
+            return 0;
+        }
+    }
+}
